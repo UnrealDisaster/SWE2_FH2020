2c31b6219429520648a677e9ffa351df68a6dcb9
Marius Hochwald <marius.hochwald@gmail.com>
2020-06-15 13:09:44 +0200

    beginning of singleton

---
+++ SWE2_FH2020/DB/DBConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace SWE2_FH2020.DB
+{
+    public sealed class DBConnection
+    {
+        DBConnection()
+        {
+        }
+        private static readonly object padlock = new object();
+        private static DBConnection instance = null;
+        public static DBConnection Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DBConnection();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public int DBConnectidasdasdasd()
+        {
+            Console.WriteLine("WTF");
+            return 5;
+        }
+
+    }
+}
